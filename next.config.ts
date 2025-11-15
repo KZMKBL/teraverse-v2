59d0f465af581ed39056e551e2acf2afd6d48d0d
Kazım <112664181+KZMKBL@users.noreply.github.com>
2025-11-15 13:08:38 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
+  // 👇 KRİTİK EKLEME
+  transpilePackages: ['gigaverse-sdk', 'gigaverse-engine'],
+  // 👆 KRİTİK EKLEME
+
   compiler: {
     removeConsole: process.env.NODE_ENV === 'production' ? { exclude: ['error', 'warn'] } : false,
   },
